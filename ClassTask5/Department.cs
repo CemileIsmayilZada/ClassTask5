ccead60b5f3176a23a12057474ff20d42f2edf68
CemileIsmayilZade <5cctf7f@code.edu.az>
2022-11-25 13:18:13 +0400

    task

---
+++ ClassTask5/Department.cs
@@ -0,0 +1,55 @@
+﻿using ClassTask5.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask5
+{
+    internal class Department
+    {
+        public static int count = 0;
+        public string Name { get; set; }
+        public int EmployeeLimit { get; set; }
+
+
+        public Employee[] employees;
+        public Department(string name, int employeeLimit)
+        {
+            Name = name;
+            EmployeeLimit = employeeLimit;
+            employees = new Employee[employeeLimit];
+
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            count++;
+            if (count <= EmployeeLimit)
+            {
+                for (int i = 0; i < EmployeeLimit; i++)
+                {
+                    employees[i] = employee;
+                }
+            }
+            else
+            {
+                throw new CapacityLimitException("You can not add more Employee");
+            }
+
+
+        }
+        public Employee this[int index]
+        {
+            get
+            {
+
+            }
+            set
+            {
+                if(value==)
+            }
+        }
+    }
+}
---
+++ ClassTask5/Employee.cs
@@ -0,0 +1,39 @@
+﻿using ClassTask5.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask5
+{
+    internal class Employee:IPerson
+    {
+        private static int _id;
+        public int Id { get; }
+        public int Salary { get; set; }
+        public string Name { get ; set; }
+        
+        public int Age { get; set; }
+
+        public Employee(string name,int age,int salary)
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+            _id++;
+            Id = _id;
+        }
+
+        public  Employee ShowInfo()
+        {
+            return this;
+        }
+        public override string ToString()
+        {
+            return $"ID: {Id}\nSalary: {Salary}\nName :{Name}\nAge: {Age}";
+        }
+
+        
+    }
+}
